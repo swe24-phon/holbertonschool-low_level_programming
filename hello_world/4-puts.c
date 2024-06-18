73ffa5738f6ca62426603bc9ff52d09edf986ac4
phon xay <majic42@hotmail.com>
2024-06-18 05:59:04 +0000

    use puts to print to stdout, newline by default modify to conform with Betty linter

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+
 /**
- *Use puts to output string to stdout 
+ *Use puts to output string to stdout
  *with newline by default
+ *
+ * Description: print to standard io
+ * return 0
  */
 
 int main(void)
@@ -9,4 +13,3 @@ int main(void)
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
- 
