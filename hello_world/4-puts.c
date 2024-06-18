23e27ed0ab53c93cf49749317c7b11023ad5e18f
phon xay <majic42@hotmail.com>
2024-06-18 05:37:29 +0000

    use puts to print to stdout, newline by default modify

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,2 +1,12 @@
-#!/bin/bash
-puts("Programming is like building a multilingual puzzle")
+#include <stdio.h>
+/**
+ * Use puts to output string to stdout 
+ * with newline by default
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
+ 
