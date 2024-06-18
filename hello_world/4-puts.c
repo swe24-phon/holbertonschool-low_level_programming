44f1ad6a69cc74525ed4cd406984a7ff1ff7fddb
phon xay <majic42@hotmail.com>
2024-06-18 05:48:32 +0000

    use puts to print to stdout, newline by default modify format to conform with Betty checker

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * Use puts to output string to stdout 
- * with newline by default
+ *Use puts to output string to stdout 
+ *with newline by default
  */
 
 int main(void)
