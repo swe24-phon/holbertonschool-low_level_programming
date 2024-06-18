f20f61f88099428477001fad43525488f4b650a3
phon xay <majic42@hotmail.com>
2024-06-18 09:46:26 +0000

    first commit modify2

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -7,16 +7,16 @@
 
 int main(void)
 {
-	char c;
+	char c="a"i;
 	int i;
 	long int li;
 	long long int lli;
 	float f;
 
-	printf("Size of a char: %c byte(s)", c);
-	printf("Size of a int: %d byte(s)", i);
-	printf("Size of a long int: %ld byte(s)", li);
-	printf("Size of a long long int: %lld byte(s)", lli);
+	printf("Size of a char: %c byte(s)\n", c);
+	printf("Size of a int: %d byte(s)\n", i);
+	printf("Size of a long int: %ld byte(s)\n", li);
+	printf("Size of a long long int: %lld byte(s)\n", lli);
 	printf("Size of a float: %f byte(s)", f);
 	return (0);
 }
