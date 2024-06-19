e40715f6c048cd73cf4e0976c9cb5de55f3fcc1e
phon xay <majic42@hotmail.com>
2024-06-19 07:07:28 +0000

    first commit

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - list out lower case alphabet
+ *
+ * Return 0 on success
+ */
+
+int main(void)
+{
+	char ch = 'a';
+	for (ch; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
