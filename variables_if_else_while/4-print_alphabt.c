28ddf41eaf3f487095eefae91685d534af4983a3
phon xay <majic42@hotmail.com>
2024-06-19 11:05:29 +0000

    first commit

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - list out lower case alphabet
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	char lowercap = 'a';
+
+	while (lowercap <= 'z')
+	{
+		if (lowercap != 'e' && lowercap != 'q')
+				putchar(lowercap);
+		lowercap++;
+	}
+	putchar('\n');
+	return (0);
+}
