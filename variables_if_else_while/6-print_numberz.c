09bb5f7e99c607db55610dc226d36d5cc262ab1a
phon xay <majic42@hotmail.com>
2024-06-19 11:48:41 +0000

    first commit3

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - list out number 0 to 9
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	char numchar = '0';
+
+	while (numchar <= '9')
+	{
+			putchar(numchar);
+			numchar++;
+	}
+	putchar('\n');
+	return (0);
+}
