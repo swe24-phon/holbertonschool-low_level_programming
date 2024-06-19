9d09d58b558b0d1e6f2df212f2be51612abd99a5
phon xay <majic42@hotmail.com>
2024-06-19 11:39:10 +0000

    first commit

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - list out digit 1 to 9
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	char numchar = '1';
+
+	while (numchar <= '9')
+	{
+		putchar(numchar);
+		numchar++;
+	}
+	putchar('\n');
+	return (0);
+}
