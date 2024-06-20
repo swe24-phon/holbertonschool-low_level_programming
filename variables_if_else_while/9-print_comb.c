098241e1a7aed17145f57cc2f5cb3372bd8e8826
phon xay <majic42@hotmail.com>
2024-06-20 05:55:53 +0000

    first commit

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - list out decimal number without using char type
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	int number = 48;
+
+	while (number <= 57)
+	{
+		putchar(number);
+		putchar(44);
+		putchar(32);
+		number++;
+	}
+	putchar('\n');
+	return (0);
+}
