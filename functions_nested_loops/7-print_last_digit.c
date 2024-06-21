26e06144d947fcffbf5a00da035227ada7a2ec08
phon xay <majic42@hotmail.com>
2024-06-21 09:54:36 +0000

    first commit

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_last_digit - find the last digit
+ *@n : number to be checked
+ *
+ * Return: true on sucess
+ *
+ */
+
+int print_last_digit(int n)
+{
+	int lastdigit;
+
+	if (n > 0)
+	{
+		return (n % 10);
+	}
+
+	else if (n == 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-1 * (n % 10));
+	}
+}
