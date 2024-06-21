07455a6647fe6a62ba783b13bbcaa1c4cd4a8c21
phon xay <majic42@hotmail.com>
2024-06-21 07:40:51 +0000

    2nd commit

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,19 +1,18 @@
 #include "main.h"
 
 /**
- * print_alphabet - call _putchar to print the lower case alphabet
+ * print_alphabet_x10 - call _putchar to print the lowercase alphabet 10 times
  *
  * Return: 0 on sucess
  *
  */
 
-void print_alphabet_x10(void);
-
+void print_alphabet_x10(void)
 {
 	int loop_count = 0;
 	char letter = 'a';
 	
-	while (loop_count < 10;)
+	while (loop_count < 10)
 	{
 		while (letter <= 'z')
 		{
