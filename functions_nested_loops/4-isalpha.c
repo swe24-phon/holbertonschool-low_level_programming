c0ded12215afddc8cd93aa543e36b667d929da13
phon xay <majic42@hotmail.com>
2024-06-21 08:55:20 +0000

    first commit

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isalpha - checking for all alphabet types
+ *@letter : character to be checked
+ *
+ * Return: true on sucess
+ *
+ */
+
+int _isalpha(int letter)
+{
+return (letter >= 97 && letter <= 122) || (letter >= 65 && letter <= 90);
+}
