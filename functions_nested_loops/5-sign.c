c2386cab0bc1f1cca20d00a1b86dbeb7823975a8
phon xay <majic42@hotmail.com>
2024-06-21 09:09:51 +0000

    first commit

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_sign - checking the sign of a number
+ *@i : character to be checked
+ *
+ * Return: true on sucess
+ *
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return(1);
+	}
+	else if (n = 0)
+	{
+		return(0);
+	}
+	else
+	{
+		return(-1);
+	}
+}
