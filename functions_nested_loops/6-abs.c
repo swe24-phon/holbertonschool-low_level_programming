5e3634c18977f7b3048f8620e987a6edb0febf4f
phon xay <majic42@hotmail.com>
2024-06-21 09:39:31 +0000

    first commit

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _abs - change to absolute number format
+ *@n : number to be checked
+ *
+ * Return: true on sucess
+ *
+ */
+
+int _abs(int n) 
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+
+	else if (n == 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-1*n);
+	}
+}
