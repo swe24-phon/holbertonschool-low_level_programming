b2766a27983af144a7870e4805906a48a9eef98a
phon xay <majic42@hotmail.com>
2024-06-23 11:50:05 +0000

    add counters

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -7,17 +7,44 @@
 
 void jack_bauer(void)
 {
-	int hour_tenth = 49;
-	int hour_unit = 49;
-	int minute_tenth = 49;
-	int minute_unit = 49;
+	int hour_tenth = 0;
+	int hour_unit = 0;
+	int minute_tenth = 0;
+	int minute_unit = 0;
 
+	if (hour_tenth <= 2)
 	{
-	_putchar(hour_tenth);
-	_putchar(hour_unit);
-	_putchar(58);
-	_putchar(minute_tenth);
-	_putchar(minute_unit);
+		while (hour_tenth <= 2)
+		{
+	if (hour_unit <= 4)
+	{
+                while (hour_unit <= 4)
+                {
+	if (minute_tenth <= 5)
+        {
+                while (minute_tenth <= 5)
+                {
+	if (minute_unit <= 9)
+        {
+                while (minute_unit <= 9)
+                {
+			_putchar(hour_tenth);
+			_putchar(hour_unit);
+			_putchar(58);
+			_putchar(minute_tenth);
+			_putchar(minute_unit);
+			minute_tenth++;
+		}
+	}
+		}
+			minute_tenth++;	
+	}
+		}
+			hour_unit++;
 	}	
+		}
+			hour_tenth++;
+	}	
+
 	_putchar('\n');
 }
