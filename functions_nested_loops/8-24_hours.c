c71f9b7074e5877c2ac42a63f6e60b38686cf65c
phon xay <majic42@hotmail.com>
2024-06-23 11:18:13 +0000

    modify

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -3,23 +3,23 @@
 /**
  * jack_bauer - print out 24:00 hours
  *
- * Return: true on sucess
+ * Return: 0 on sucess
  *
  */
 
-void jack_bauer(void);
+void jack_bauer(void)
 {
 	int hour_tenth 49;
-	int hour_tenth 49;
-	int hour_tenth 49;
-	int hour_tenth 49;
+	int hour_unit 49;
+	int minute_tenth 49;
+	int minute_unit 49;
 
 	{
 	_putchar(hour_tenth);
 	_putchar(hour_unit);
 	_putchar(58);
 	_putchar(minute_tenth);
-	_putchar(hour_unit);
+	_putchar(minute_unit);
 	}	
 _putchar('\n');
 return (0);
