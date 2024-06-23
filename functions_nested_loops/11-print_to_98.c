49e721a36f739e5e08291916a8d1945814fac29b
phon xay <majic42@hotmail.com>
2024-06-23 15:32:53 +0000

    fix style

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -13,7 +13,7 @@
 void print_to_98(int n)
 {
 	if (n < 98)
-	while ( n < 98)
+	while (n < 98)
 	{
 		printf("%i, ", n);
 		n++;
@@ -26,6 +26,5 @@ void print_to_98(int n)
 			n--;
 		}
 	}
-	printf("98\n");
-			
+	printf("98\n");		
 }
