b488cf8683764b09777de585fdfa38e84dbb4f6f
phon xay <majic42@hotmail.com>
2024-06-23 11:11:41 +0000

    first commit

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * jack_bauer - print out 24:00 hours
+ *
+ * Return: true on sucess
+ *
+ */
+
+void jack_bauer(void);
+{
+	int hour_tenth 49;
+	int hour_tenth 49;
+	int hour_tenth 49;
+	int hour_tenth 49;
+
+	{
+	_putchar(hour_tenth);
+	_putchar(hour_unit);
+	_putchar(58);
+	_putchar(minute_tenth);
+	_putchar(hour_unit);
+	}	
+_putchar('\n');
+return (0);
+}
