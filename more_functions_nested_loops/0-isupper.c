a264a6b9ca36e32105e0936e75bdfd26db687078
phon xay <majic42@hotmail.com>
2024-06-24 05:13:45 +0000

    fix comment

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -4,6 +4,15 @@
 #define CAP_A 65
 #define CAP_Z 90
 
+/**
+ * _isupper - checking the input character is upper case
+ *@c - chracter input
+ *
+ * Return: 1 for true else 0 for fault
+ *
+ */
+
+
 int _isupper(int c)
 {
 	if((c >= CAP_A) && (c <= CAP_Z))
