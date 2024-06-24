53d18f9ff326c62871b6e19f85ea54c14858ec53
phon xay <majic42@hotmail.com>
2024-06-24 05:07:09 +0000

    first commit

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+#define CAP_A 65
+#define CAP_Z 90
+
+int _isupper(int c)
+{
+	if((c >= CAP_A) && (c <= CAP_Z))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	{
+}
