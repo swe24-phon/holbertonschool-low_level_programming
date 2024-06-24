2e19da5e5e65c002de318e8c4912443219d15054
phon xay <majic42@hotmail.com>
2024-06-24 05:46:39 +0000

    first commit

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isdigit - checking the input character is a number
+ * @a : integer value input
+ * @b : integer value input
+ *
+ * Return: result
+ *
+ */
+
+
+int mul(int a, int b)
+{
+	return (a*b);
+}
