30f87bec9032db69580324952d000e39beab07a2
phon xay <majic42@hotmail.com>
2024-06-25 03:45:34 +0000

    fix logic2

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -16,10 +16,10 @@ void print_diagonal(int n)
 	{
 		for (i = 0; i < n; i++)
 		{
-		for (j = 0; j < i; j++)
-		{
-			 _putchar(' ');
-		}
+			for (j = 0; j < i; j++)
+			{
+				 _putchar(' ');
+			}
 			_putchar('\\');
 			_putchar('\n');
 		}
