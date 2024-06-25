45ecbdc19e4b7142f080836103dced2efd841bae
phon xay <majic42@hotmail.com>
2024-06-25 01:48:54 +0000

    fix style2

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -3,7 +3,7 @@
 
 #define ZERO 48
 /**
- * print_most_numbers - print a sequence of 0 to 9, but not 2 and 4
+ * more_numbers - print a sequence of 0 to 14
  *
  */
 
@@ -17,7 +17,7 @@ void more_numbers(void)
 	{
 		counter = 0;
 		while (counter <= 14)
-		{	
+		{
 			if (counter > 9)
 			{
 				_putchar(ZERO + counter / 10);
