5d8c1144ab8ba026d2aa6cc7bca1c7c45a1452c4
phon xay <majic42@hotmail.com>
2024-06-25 02:50:19 +0000

    first commit

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_diagonal - print a diagonal line with length n
+ *@n : integer number for line length
+ *
+ */
+
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (!(n <= 0))
+	{
+		for (i = 1; i <= n; i++)
+		{
+			for (j = 1; j <= i; j++)
+			{
+				 _putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
