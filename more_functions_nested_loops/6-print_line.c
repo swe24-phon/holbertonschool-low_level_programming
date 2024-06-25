64b92cf0522060355835e655af3af7f3a049a46a
phon xay <majic42@hotmail.com>
2024-06-25 02:22:13 +0000

    first commit

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *  print_line - print a line with length n
+ *
+ */
+
+
+void print_line(int n)
+{
+	int i;
+
+	if (!(n <= 0))
+	{
+		for( i = 1; i <= n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
