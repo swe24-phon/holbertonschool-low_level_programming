f134c873677426c95baf3025a63ed727706e8418
phon xay <majic42@hotmail.com>
2024-07-01 05:19:45 +0000

    first commit

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * puts_half - print last half
+ *
+ * @str: string of character
+ *
+ */
+
+void puts_half(char *str)
+{
+	int length, half = 0;
+	char *temp = str;
+
+	while (temp[length] != '\0')
+	{
+		length++;
+	}
+	half = length/2;
+	if (length % 2 == 1)
+		half++;
+	while (half < length)
+	{
+		_putchar(temp[half]);
+		half++;
+	}
+		_putchar('\n');
+}
