5511357705820c9e732cc13b7109ad03ec289d2a
phon xay <majic42@hotmail.com>
2024-07-01 01:07:40 +0000

    update rev

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -13,14 +13,15 @@ void print_rev(char *s)
 {
 	int length = 0;
 	int rev;
+	char *temp = s; 
 
-	while (*s != '\0')
+	while (*temp != '\0')
 	{
 		length++;
-		s++;
+		temp++;
 	}
 	rev = length;
-	while (rev >= 0)
+	while (rev--)
 	{
 		_putchar(*(s + rev));
 	}
