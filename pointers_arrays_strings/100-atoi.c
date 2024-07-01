fbc76f454445485a6d2dea25942be4748281b0de
phon xay <majic42@hotmail.com>
2024-07-01 07:58:18 +0000

    move to git first

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strcpy - copy string
+ *
+ * @dest: destination to copy to
+ * @src: orginal source
+ *
+ * Return: 0 on sucess
+ *
+ */
+
+int _atoi(char *s)
+{
+	int i = 0;
+	int neg = 1;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == 45)
+			neg *= -1;
+		if (s[i] >= 48 && s[i] <= 57)
+	}
+	return (0);
+}
