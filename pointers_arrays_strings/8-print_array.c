e09cecd804290327c1a0c73390927eab60ca28c5
phon xay <majic42@hotmail.com>
2024-07-01 06:03:45 +0000

    first commit

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_array - print array elements
+ *
+ * @a: array elements
+ * @n: up to nth elements
+ *
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i != (n-1))
+		{
+			printf(", ");
+		}
+	}
+		printf("\n");
+}
