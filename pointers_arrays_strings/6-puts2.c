3630b5408d1863e966d56653db248964df9c99ee
phon xay <majic42@hotmail.com>
2024-07-01 04:23:07 +0000

    modify add temp as copy to s

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts2 - print every other character
+ *
+ * @str: string of char pointer
+ *
+ */
+void puts2(char *str) 
+{
+    int loc = 0;
+    char *temp = str;
+
+    while (*temp != '\0') 
+    {
+        if (loc % 2 == 0) 
+	{
+            _putchar(*temp);
+        }
+        loc++;
+        temp++;
+    }
+    _putchar('\n');
+}
