860407830b82ef88947ba214cfe6bbf234a20761
phon xay <majic42@hotmail.com>
2024-07-03 13:29:49 +0000

    First commit

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * cap_string - all words change to capital letter
+ *
+ * @s: string of words
+ *
+ * Return: result
+ */
+
+char *cap_string(char *s)
+{
+	int n = 0;
+
+	while (s[n] != '\0')
+	{
+		if (s[n] >= 'a' && s[n] <= 'z')
+		{
+			if (s[n] == 0)
+			{
+				s[n] -= 32;
+			}
+		if ((s[n - 1] == ' ' || s[n - 1] == '\n' ||
+			s[n - 1] == ',' || s[n - 1] == ';' ||
+			s[n - 1] == '.' || s[n - 1] == '!' ||
+			s[n - 1] == '?' || s[n - 1] == '"' ||
+			s[n - 1] == '(' || s[n - 1] == ')' ||
+			s[n - 1] == '{' || s[n - 1] == '}' ||
+			s[n - 1] == '\t') && (s[n] >= 'a' && s[n] <= 'z'))
+		{
+			s[n] -= 32;
+		}
+		}
+	n++;
+	}
+	return (s);
+}
