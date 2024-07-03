c3b035e598d907ac1181ee5543288c0e632b60cb
phon xay <majic42@hotmail.com>
2024-07-03 02:16:04 +0000

    First commit

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strcat - concating two strings
+ *
+ * @dest: string
+ * @src: string
+ * @n: number of times
+ *
+ * Return: result
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	char *result = dest;
+	int len = 0;
+
+	while (*dest)
+	{
+		dest++;
+	}
+	while (*src)
+	{
+		if (len < n)
+		{
+		*dest = *src;
+		}		
+		len++;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (result);
+}
