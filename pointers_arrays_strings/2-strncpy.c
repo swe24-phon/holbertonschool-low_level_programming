5e3a928faaed382ae06d60b2179fbeeea3308cd2
phon xay <majic42@hotmail.com>
2024-07-03 03:17:01 +0000

    first commit

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * *_strncpy - copy the elements as specified
+ *
+ * @dest: string
+ * @src: string
+ * @n: number of character to copy
+ *
+ * Return: result
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	char *result = dest;
+	int len = 0;
+
+	while (*src)
+	{
+		if (len < n)
+		{
+		*dest = *src;
+		}
+		len++;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (result);
+}
