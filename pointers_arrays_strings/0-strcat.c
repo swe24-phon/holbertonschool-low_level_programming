556a76a2c40bb7abb0d51f23b7646f8d22b14eb3
phon xay <majic42@hotmail.com>
2024-07-03 01:18:18 +0000

    first commit

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strcat - concating two strings
+ *
+ * @dest: string
+ * @src: string
+ *
+ * Return: result
+ */
+
+char *_strcat(char *dest, char *src)
+{
+    char *result = dest;
+
+    while (*dest)
+    {
+        dest++;
+    }
+
+        while (*src)
+    {
+        *dest = *src;
+        dest++;
+        src++;
+    }
+
+       *dest = '\0';
+
+    return result;
+}
