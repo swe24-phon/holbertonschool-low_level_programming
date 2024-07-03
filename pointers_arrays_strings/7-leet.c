8b8e0019d949405261f1baf939898b3444dd358c
phon xay <majic42@hotmail.com>
2024-07-03 15:19:29 +0000

    first commit

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * leet - encrypting message
+ *
+ * @s: string of words
+ *
+ * Return: result
+ */
+
+char *leet(char *s)
+{
+	int i, j;
+	char *drp = "aAeEoOtTlL";
+	char *crp = "4433007712";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (s[i] == drp[j])
+			{
+				s[i] = crp[j];
+			}
+		}
+	}
+	return (s);
+}
