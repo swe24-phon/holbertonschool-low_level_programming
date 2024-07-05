60bef7b361829e8c50f9b5a50089d33859f856df
phon xay <majic42@hotmail.com>
2024-07-05 11:02:18 +0000

    First commit

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * @s: string
+ * @accept: pattern to search for
+ *
+ * Return: Always 0.
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	while (*s != '\0')
+	{
+		char *a = accept;
+
+		while (*a != '\0')
+		{
+			if (*s == *a)
+			{
+				return (s);
+			}
+			a++;
+		}
+		s++;
+	}
+	return (NULL);
+}
