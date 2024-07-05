41baeedc95717ba0b9a162ba106cfc8b09c44997
phon xay <majic42@hotmail.com>
2024-07-05 02:04:20 +0000

    First commit

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _memcpy - copy from src to dest buffer in hexa
+ * @dest: the address of memory of src's copy
+ * @src: the address of memory to copy to
+ * @n: the number of item to be copied
+ *
+ * Return: dest values.
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+		i++;
+	}
+	return (dest);
+}
