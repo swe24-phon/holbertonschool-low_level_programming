72070d1be3fdf114e8de6110235a376d69432d72
phon xay <majic42@hotmail.com>
2024-07-05 04:54:45 +0000

    fix style

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -15,12 +15,13 @@ char *_strchr(char *s, char c)
 	{
 		if (*s == c)
 		{
-			return s;
+			return (s);
 		}
 		s++;
 	}
-	if (c == '\0') {
-		return s;
+	if (c == '\0')
+	{
+		return (s);
 	}
-	return NULL;
+	return (NULL);
 }
