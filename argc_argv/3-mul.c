c2761283567f66559d835450bec5581dc55bb07e
phon xay <majic42@hotmail.com>
2024-07-10 12:27:48 +0000

    First commit

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main program
+ *
+ * @argc: argument counter
+ * @argv: actual arguments
+ *
+ * Return: 0 for sucess
+ */
+
+int main(int argc, char *argv[])
+{
+	int result = 0;
+
+	if (argc <= 3)
+	{
+		printf("Error");
+		return (1);
+	}
+	else
+	{
+		result = atoi(argv[2]) * atoi(argv[1]);
+		printf("%d\n", result);
+	}
+	return (0);
+}
+
