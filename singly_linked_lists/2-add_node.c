4e2d47fb66f1a1a890388120f567796e4e0b5ac6
phon xay <majic42@hotmail.com>
2024-08-04 10:22:16 +0000

    fix betty style4

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -15,7 +15,7 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *new_node;
 	char *new_str;
 	unsigned int len = 0;
-	
+
 	new_node = (list_t *)malloc(sizeof(list_t));
 	if (new_node == NULL)
 		return (NULL);
