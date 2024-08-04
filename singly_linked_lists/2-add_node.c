e1de3374b1fc06ccdf5c2d46019189b60a7f713e
phon xay <majic42@hotmail.com>
2024-08-04 10:09:29 +0000

    fix betty style

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -4,11 +4,11 @@
 #include "lists.h"
 
 /**
- * add_node - add a node
- * @head: pointer to the list_t list
- * @str: str to add to the node
+ * add_node - Adds a new node at the beginning of a list_t list.
+ * @head: Double pointer to the head of the list.
+ * @str: String to be added to the new node.
  *
- * Return: the new node, or NULL if it failed
+ * Return: Address of the new element, or NULL if it failed.
  */
 list_t *add_node(list_t **head, const char *str)
 {
@@ -16,7 +16,7 @@ list_t *add_node(list_t **head, const char *str)
     char *new_str;
     unsigned int len = 0;
 
-    new_node = malloc(sizeof(list_t));
+    new_node = (list_t *)malloc(sizeof(list_t));
     if (new_node == NULL)
         return (NULL);
 
@@ -31,10 +31,9 @@ list_t *add_node(list_t **head, const char *str)
         len++;
 
     new_node->str = new_str;
-    new_node->len = len;  
+    new_node->len = len;
     new_node->next = *head;
     *head = new_node;
 
     return (new_node);
 }
-
