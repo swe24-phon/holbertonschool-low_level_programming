1fffb04098b9b6d159c49a47793224d5891f8d94
phon xay <majic42@hotmail.com>
2024-08-04 10:19:56 +0000

    fix betty style2

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -12,28 +12,24 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-    list_t *new_node;
-    char *new_str;
-    unsigned int len = 0;
-
-    new_node = (list_t *)malloc(sizeof(list_t));
-    if (new_node == NULL)
-        return (NULL);
-
-    new_str = strdup(str);
-    if (new_str == NULL)
-    {
-        free(new_node);
-        return (NULL);
-    }
-
-    while (str[len] != '\0')
-        len++;
-
-    new_node->str = new_str;
-    new_node->len = len;
-    new_node->next = *head;
-    *head = new_node;
-
-    return (new_node);
+	list_t *new_node;
+	char *new_str;
+	unsigned int len = 0;
+	
+	new_node = (list_t *)malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_str = strdup(str);
+	if (new_str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	while (str[len] != '\0')
+		len++;
+	new_node->str = new_str;
+	new_node->len = len;
+	new_node->next = *head;
+	*head = new_node;
+	return (new_node);
 }
