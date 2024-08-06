4f8b5e9cf8689e103ab8cbe618827ba8220fdf4b
phon xay <majic42@hotmail.com>
2024-08-06 16:05:51 +0000

    change rollback

--- bit_manipulation/3-set_bit.c
+++ bit_manipulation/3-set_bit.c
@@ -12,7 +12,9 @@
 int set_bit(unsigned long int *n, unsigned int index)
 {
 	if (index >= sizeof(unsigned long int) * 8)
+	{
 		return (-1);
-	*n |= 1UL << (index % ((sizeof(unsigned long int) * 8)));
-	return (0);
+	}
+	*n |= 1UL << index;
+	return (1);
 }
