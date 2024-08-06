fab7bf2e4c68f65a6c8ea98e769cc9438720e46d
phon xay <majic42@hotmail.com>
2024-08-06 15:24:32 +0000

    First commit

---
+++ bit_manipulation/3-set_bit.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * set_bit - set bit to 1 at index
+ *
+ * @n: integer pointer
+ * @index: desired position
+ * Return: integer number
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index < 64)
+	{
+		*n |= 1UL << index;
+		return (0);
+	}
+	return (*n);
+}
---
+++ bit_manipulation/4-clear_bit.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * clear_bit - clear bit to 0 at index
+ *
+ * @n: integer pointer
+ * @index: desired position
+ * Return: integer number
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= 64)
+		return -1;
+	*n = *n & ~(1UL << index);
+	return 1;
+}
