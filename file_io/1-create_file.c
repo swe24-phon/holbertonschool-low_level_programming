ce7fbce430e495f829cd890656769a9bb29d5b35
phon xay <majic42@hotmail.com>
2024-08-08 10:57:54 +1000

    fix  while logic

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -14,15 +14,14 @@ int create_file(const char *filename, char *text_content)
 	if (text_content != NULL)
 	{
 		while (text_content[text_length])
-		{
 			text_length++;
-			bytes_written = write(fd, text_content, text_length);
+		
+		bytes_written = write(fd, text_content, text_length);
 		if (bytes_written == -1 || (size_t)bytes_written
 			!= text_length)
 		{
 			close(fd);
 			return (-1);
-		}
 		}
         }
         close(fd);
