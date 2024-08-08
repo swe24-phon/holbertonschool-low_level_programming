575be2084e172ab476b005c2f596eac382400fed
phon xay <majic42@hotmail.com>
2024-08-08 13:28:13 +1000

    fix style and logic

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -21,9 +21,10 @@ void print_error_and_exit(const char *message)
  * @buffer: Buffer to hold the data being read and written.
  * @bytes_read: Number of bytes read and written.
  */
-void read_and_write(int fd_from, int fd_to, char *buffer, size_t bytes_read)
+void read_and_write(int fd_to, char *buffer, ssize_t bytes_read)
 {
-	if (write(fd_to, buffer, bytes_read) != bytes_read) {
+	if (write(fd_to, buffer, bytes_read) != bytes_read)
+	{
 		print_error_and_exit("Error: Can't write to file");
 	}
 }
@@ -50,25 +51,27 @@ void close_files(int fd1, int fd2)
  */
 int main(int argc, char **argv)
 {
+	char *file_to, *file_from;
+	int fd_from, fd_to;
+	char buffer[BUFFER_SIZE];
+	ssize_t bytes_read;
+	
 	if (argc != 3) {
 		print_error_and_exit("Error: Please provide source and destination file names");
 	}
 
-	char *file_to = argv[2];
-	char *file_from = argv[1];
+	file_to = argv[2];
+	file_from = argv[1];
 
-	int fd_from = open(file_from, O_RDONLY);
-	int fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	fd_from = open(file_from, O_RDONLY);
+	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0644);
 
 	if (fd_from == -1 || fd_to == -1) {
 		print_error_and_exit("Error: Can't open file");
 	}
 
-	char buffer[BUFFER_SIZE];
-	size_t bytes_read;
-
 	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0) {
-		read_and_write(fd_from, fd_to, buffer, bytes_read);
+		read_and_write(fd_to, buffer, bytes_read);
 	}
 
 	if (bytes_read == -1) {
