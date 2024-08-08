dfd10d2bdeda3c921167fe60db86236fa54aabc9
phon xay <majic42@hotmail.com>
2024-08-08 10:52:00 +1000

    fix style

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -2,25 +2,26 @@
 
 int create_file(const char *filename, char *text_content)
 {
-        int fd;
-        
-        ssize_t bytes_written = 0;
-        size_t text_length = 0;
-        if (filename == NULL)
-                return (-1);
-        fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-        if (fd == -1)
-                return (-1);
-        if (text_content != NULL)
-        {
-                while (text_content[text_length])
-                        text_length++;
-                bytes_written = write(fd, text_content, text_length);
-                if (bytes_written == -1 || (size_t)bytes_written != text_length)
-                {
-                        close(fd);
-                        return (-1);
-                }
+	int fd;
+
+	ssize_t bytes_written = 0;
+	size_t text_length = 0;
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+	if (text_content != NULL)
+	{
+		while (text_content[text_length])
+			text_length++;
+			bytes_written = write(fd, text_content, text_length);
+		if (bytes_written == -1 || (size_t)bytes_written
+			!= text_length)
+		{
+			close(fd);
+			return (-1);
+		}
         }
         close(fd);
         return (1);
