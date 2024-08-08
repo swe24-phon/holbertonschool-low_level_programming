77bc5f62246dc7dc50197688c89ecbdb95e39cd2
phon xay <majic42@hotmail.com>
2024-08-09 09:38:31 +1000

    try new way

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,82 +1,88 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include "main.h"
 
-#define BUFFER_SIZE 1024
 
-/**
- * print_error_and_exit - Prints an error message to POSIX stderr and exits.
- *
- * @message: The error message to print.
- */
-void print_error_and_exit(const char *message)
+#define BUFFER_SIZE	1024
+
+/*
+** Prints the usage message and exits the program.
+*/
+void	print_usage(void)
 {
-	dprintf(STDERR_FILENO, "%s\n", message);
-	exit(0);
+	dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+	exit(97);
 }
 
-/**
- * read_and_write - Reads data from one file and writes it to another.
- *
- * @fd_from: File descriptor of the source file.
- * @fd_to: File descriptor of the destination file.
- * @buffer: Buffer to hold the data being read and written.
- * @bytes_read: Number of bytes read and written.
- */
-void read_and_write(int fd_to, char *buffer, ssize_t bytes_read)
+/*
+** Prints an error message and exits the program when a file cannot be read.
+*@filename: file name
+*/
+void	error_cant_read(char *filename)
 {
-	if (write(fd_to, buffer, bytes_read) != bytes_read)
+	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
+	exit(98);
+}
+
+/*
+** Prints an error message and exits the program when a file cannot be written.
+*@filename: file name
+*/
+void	error_cant_write(char *filename)
+{
+	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
+	exit(99);
+}
+
+/*
+** Prints an error message and exits the program when a file descriptor cannot be closed.
+*@fd: file descriptor
+*/
+void	error_cant_close(int fd)
+{
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+	exit(100);
+}
+
+/*
+** The main function.
+*/
+int	main(int argc, char *argv[])
+{
+	int		fd_from;
+	int		fd_to;
+	ssize_t		rd;
+	ssize_t		wr;
+	char		buffer[BUFFER_SIZE];
+
+	if (argc != 3)
+		print_usage();
+
+	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from == -1)
+		error_cant_read(argv[1]);
+
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd_to == -1)
+		error_cant_write(argv[2]);
+
+	while ((rd = read(fd_from, buffer, BUFFER_SIZE)) > 0)
 	{
-		print_error_and_exit("Error: Can't write to file");
-	}
-}
-
-/**
- * close_files - Closes two file descriptors.
- *
- * @fd1: First file descriptor to close.
- * @fd2: Second file descriptor to close.
- */
-void close_files(int fd1, int fd2)
-{
-	if (close(fd1) == -1 || close(fd2) == -1) {
-		print_error_and_exit("Error: Can't close file descriptor");
-	}
-}
-
-/**
- * main - main file.
- * @argc: argument count.
- * @argv: additional options.
- *
- * Return: 0 on success and non-zero on failure.
- */
-int main(int argc, char **argv)
-{
-	char *file_to, *file_from;
-	int fd_from, fd_to;
-	char buffer[BUFFER_SIZE];
-	ssize_t bytes_read;
-	
-	if (argc != 3) {
-		print_error_and_exit("Error: Please provide source and destination file names");
+		wr = write(fd_to, buffer, rd);
+		if (wr == -1 || wr != rd)
+			error_cant_write(argv[2]);
 	}
 
-	file_to = argv[2];
-	file_from = argv[1];
+	if (rd == -1)
+		error_cant_read(argv[1]);
 
-	fd_from = open(file_from, O_RDONLY);
-	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (close(fd_from) == -1)
+		error_cant_close(fd_from);
 
-	if (fd_from == -1 || fd_to == -1) {
-		print_error_and_exit("Error: Can't open file");
-	}
+	if (close(fd_to) == -1)
+		error_cant_close(fd_to);
 
-	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0) {
-		read_and_write(fd_to, buffer, bytes_read);
-	}
-
-	if (bytes_read == -1) {
-		print_error_and_exit("Error reading from file");
-	}
-	close_files(fd_from, fd_to);
-	return 0;
+	return (0);
 }
